a438cfcd0d2a8aff345bc12fd9a762a202ca9fd7
Artem439 <149186640+Artem439@users.noreply.github.com>
2026-01-21 19:40:54 +0300

    Fix the GroundDetector class

--- Assets/Game/Scripts/Entities/Player/Movement/GroundDetector.cs
+++ Assets/Game/Scripts/Entities/Player/Movement/GroundDetector.cs
@@ -7,17 +7,11 @@ namespace Game.Scripts.Entities.Player
         [SerializeField] private LayerMask _groundLayer;
         [SerializeField] private Transform _rayOrigin;
         
-        [SerializeField] private float _checkDistance = 0.1f;
-
-        private bool _isGrounded;
+        [SerializeField] private float _rayDistance = 0.1f;
         
         public bool IsGrounded()
         {
-            RaycastHit2D hit = Physics2D.Raycast(_rayOrigin.position, Vector2.down, _checkDistance, _groundLayer);
-        
-            _isGrounded = hit.collider != null;
-            
-            return _isGrounded;
+            return Physics2D.Raycast(_rayOrigin.position, Vector2.down, _rayDistance, _groundLayer);
         }
     }
 }
