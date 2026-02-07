da2a6a5aa01c9bcc6271ff4c731b67bd14282482
Artem439 <149186640+Artem439@users.noreply.github.com>
2026-02-07 11:40:58 +0300

    Minor fix

--- Assets/Game/Scripts/Entities/Enemy/PlayerDetector.cs
+++ Assets/Game/Scripts/Entities/Enemy/PlayerDetector.cs
@@ -10,6 +10,7 @@ namespace Game.Scripts.Entities.Enemy
         
         public void OnTriggerEnter2D(Collider2D other)
         {
+            Debug.Log($"OnTriggerEnter2D {other.gameObject.name}");
             if (other.TryGetComponent(out Player.Player player))
             {
                 OnPlayerEntered?.Invoke(player.transform);
@@ -18,6 +19,7 @@ namespace Game.Scripts.Entities.Enemy
 
         public void OnTriggerExit2D(Collider2D other)
         {
+            Debug.Log($"OnTriggerExit2D {other.gameObject.name}");
             if (other.TryGetComponent(out Player.Player _))
             {
                 OnPlayerOut?.Invoke();
