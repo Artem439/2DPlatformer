110e5d839fea09dfe00643633b273d07e60e486c
Artem439 <149186640+Artem439@users.noreply.github.com>
2026-02-24 18:35:23 +0300

    add the basic implementation of the object pool

---
+++ Assets/Game/Scripts/Entities/Base/PoolBase.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Game.Scripts.Entities.Base
+{
+    public abstract class PoolBase<T> : MonoBehaviour where T : Component
+    {
+        [SerializeField] private T _potionPrefab;
+        [SerializeField] private int _capacity;
+        [SerializeField] private int _maxSize;
+        
+        private ObjectPool<T> _pool;
+    
+        protected virtual void Awake()
+        {
+            _pool = new ObjectPool<T>(
+                createFunc: () => CreateObject(),
+                actionOnGet: (obj) => OnGetObject(obj),
+                actionOnRelease: (obj) => OnReleaseObject(obj),
+                actionOnDestroy: (obj) => Destroy(obj),
+                collectionCheck: true,
+                defaultCapacity: _capacity,
+                maxSize: _maxSize);
+        }
+    
+        public T Get()
+        {
+            return _pool.Get();
+        }
+
+        public void Release(T entity)
+        {
+            _pool.Release(entity);
+        }
+
+        protected virtual T CreateObject()
+        {
+            return Instantiate(_potionPrefab);
+        }
+
+        protected virtual void OnGetObject(T entity)
+        {
+            entity.gameObject.SetActive(true);
+        }
+    
+        protected virtual void OnReleaseObject(T entity)
+        {
+            entity.gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Game/Scripts/Entities/CollectedItems/Coin/Spawner/CoinsPool.cs
+++ Assets/Game/Scripts/Entities/CollectedItems/Coin/Spawner/CoinsPool.cs
@@ -1,51 +1,7 @@
-﻿using UnityEngine;
-using UnityEngine.Pool;
-
+﻿using Game.Scripts.Entities.Base;
 namespace Game.Scripts.Entities.Coin
 {
-    public class CoinsPool : MonoBehaviour
+    public class CoinsPool : PoolBase<Coin>
     {
-        [SerializeField] private Coin _coinPrefab;
-        [SerializeField] private int _capacity;
-        [SerializeField] private int _maxSize;
-        
-        private ObjectPool<Coin> _coinsPool;
-    
-        private void Awake()
-        {
-            _coinsPool = new ObjectPool<Coin>(
-                createFunc: () => CreateObject(),
-                actionOnGet: (obj) => OnGetObject(obj),
-                actionOnRelease: (obj) => OnReleaseObject(obj),
-                actionOnDestroy: (obj) => Destroy(obj),
-                collectionCheck: true,
-                defaultCapacity: _capacity,
-                maxSize: _maxSize);
-        }
-    
-        public Coin Get()
-        {
-            return _coinsPool.Get();
-        }
-
-        public void Release(Coin coin)
-        {
-            _coinsPool.Release(coin);
-        }
-
-        private Coin CreateObject()
-        {
-            return Instantiate(_coinPrefab);
-        }
-
-        private void OnGetObject(Coin coin)
-        {
-            coin.gameObject.SetActive(true);
-        }
-    
-        private void OnReleaseObject(Coin coin)
-        {
-            coin.gameObject.SetActive(false);
-        }
     }
 }
